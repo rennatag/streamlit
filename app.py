19056a0d32a5f989c83ac74b21661cd8af8567ed
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-06 16:09:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,29 +33,29 @@ output_file = 'model.keras'
 st.title("Skintone Predictor")
 
 # Button to trigger model download
-if st.button('Download and Load Model'):
-    with st.spinner('Downloading model...'):
-        download_file_from_drive(file_url, output_file)
-        st.success('Model downloaded successfully!')
+#if st.button('Download and Load Model'):
+with st.spinner('Model loading. Please wait ...'):
+    download_file_from_drive(file_url, output_file)
+    st.success('Model downloaded successfully!')
 
-    # Check if the file exists
-    if os.path.exists(output_file):
-        # Check if the file is indeed an .h5 file by size (usually > few MBs)
-        file_size = os.path.getsize(output_file)
-        st.write(f"Downloaded file size: {file_size / (1024 * 1024)} MB")
-        
-        if file_size > 0:  # Ensuring the file is not empty
-            try:
-                with st.spinner('Loading model...'):
-                    model = load_model(output_file)
-                    st.success('Model loaded successfully!')
-                    st.write(model.summary())
-            except OSError as e:
-                st.error(f"Error loading model: {e}")
-        else:
-            st.error('Downloaded file is empty or corrupted.')
+# Check if the file exists
+if os.path.exists(output_file):
+    # Check if the file is indeed an .h5 file by size (usually > few MBs)
+    file_size = os.path.getsize(output_file)
+    #st.write(f"Downloaded file size: {file_size / (1024 * 1024)} MB")
+    
+    if file_size > 0:  # Ensuring the file is not empty
+        try:
+            with st.spinner('Loading model...'):
+                model = load_model(output_file)
+                st.success('Model loaded successfully!')
+                st.write(model.summary())
+        except OSError as e:
+            st.error(f"Error loading model: {e}")
     else:
-        st.error('Error: Model file not found.')
+        st.error('Downloaded file is empty or corrupted.')
+else:
+    st.error('Error: Model file not found.')
 
 # Image uploader
 uploaded_file = st.file_uploader("Upload an image for prediction", type=["jpg", "png", "jpeg"])
